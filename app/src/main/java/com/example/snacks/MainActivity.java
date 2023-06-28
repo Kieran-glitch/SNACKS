d246f7d7631c6276a65a98851a83e4d97e56e7f3
Kieran Ameresekere <kieraniscoolerthanu@gmail.com>
2023-06-28 21:49:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/snacks/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.snacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Hello there",BaseTransientBottomBar.LENGTH_LONG);
+            }
+        });
+    }
+}
